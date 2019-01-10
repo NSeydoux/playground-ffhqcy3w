86ff767ab811e8822707ef1fb19df4f99b7254bc
Nicolas Seydoux <nicolas.seydoux@aliceadsl.fr>
2019-01-10 06:56:21 +0100

    Added first cinema query, updated query script
    The initial example will use a cinema dataset inspired from the Neo4J tutorial. In order to make the query script more usable, now it is possible to load multiple datasets of multiple formats, and the results are displayed as a dictionnary.

--- python-project/run_query.py
+++ python-project/run_query.py
@@ -3,14 +3,14 @@ import sys
 
 g = rdflib.Graph()
 
+# The first parameter is the query, and the rest is a list of pairs <path, format>
 q_path= sys.argv[1]
-d_path= sys.argv[2]
+for i in range(2, len(sys.argv)-1):
+    g.parse(sys.argv[i], format=sys.argv[i+1])
 
-# ... add some triples to g somehow ...
-g.parse(d_path)
 with open(q_path, "r") as q_file:
     query = q_file.read()
     qres = g.query(query)
 
 for row in qres:
-    print("%s knows %s" % row)
+    print({k:str(v) for k,v in row.asdict().items()})
